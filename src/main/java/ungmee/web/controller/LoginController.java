9f0c893e5763a770ec5cf899a21f8d7c8dabef2f
Mellome <Mellome@DESKTOP-NNQ4UMN>
2019-07-09 23:39:12 +0900

    똑땍이 아빠 css때문에 빡친 날

---
+++ src/main/java/ungmee/web/controller/LoginController.java
@@ -0,0 +1,20 @@
+package ungmee.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class LoginController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		ModelAndView mv = new ModelAndView("/WEB-INF/view/user/login.jsp");
+		
+		
+		return mv;
+	}
+
+}
---
+++ src/main/java/ungmee/web/controller/SignupController.java
@@ -0,0 +1,20 @@
+package ungmee.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class SignupController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		ModelAndView mv = new ModelAndView("/WEB-INF/view/guest/signup.jsp");
+		
+		
+		return mv;
+	}
+
+}

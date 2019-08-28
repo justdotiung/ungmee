9cadf6ac6c67a660006e074a786bb0742faf9540
JHTA <JHTA@JHTA-PC>
2019-08-28 18:13:01 +0900

    지도 api 구현중

--- src/main/webapp/resource/js/couple/cource/reg.js
+++ src/main/webapp/resource/js/couple/cource/reg.js
@@ -1,10 +1,185 @@
 window.addEventListener("load",function(){
-    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    var options = { //지도를 생성할 때 필요한 기본 옵션
-        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-        level: 3 //지도의 레벨(확대, 축소 정도)
-    };
+    var mark = this.document.querySelector("#mark");
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(37.566535, 126.97796919000007), // 지도의 중심좌표
+        level: 8 // 지도의 확대 레벨 
+    }; 
+
+    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+//========================= 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+    var mapTypeControl = new kakao.maps.MapTypeControl();
+
+    // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+    // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+    // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+    var zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+//========================= 지도에 클릭 이벤트를 등록합니다
+    // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+    kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
+        
+        // 클릭한 위도, 경도 정보를 가져옵니다 
+        var latlng = mouseEvent.latLng;
+        
+        var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+        message += '경도는 ' + latlng.getLng() + ' 입니다';
+        
+        var resultDiv = document.getElementById('result'); 
+        resultDiv.innerHTML = message;
+        
+    });
+//======================= 마커 표시
+    var imageSrc = '../../resource/images/icon/sicon.png', // 마커이미지의 주소입니다    
+        imageSize = new kakao.maps.Size(50, 55), // 마커이미지의 크기입니다
+        imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+      
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+   
+    // 선을 구성하는 좌표 배열입니다. 이 좌표들을 이어서 선을 표시합니다
     
-    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    // 지도에 표시할 선을 생성합니다
+    
+    // 지도에 선을 표시합니다 
+    
+    // 지도에 클릭 이벤트를 등록합니다
+    // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+    kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
+        // 클릭한 위치에 마커를 표시합니다 
+    
+        addMarker(mouseEvent.latLng);    
+        addLin(mouseEvent.latlng.getLat(),mouseEvent.latlng.getLng());
+    });
+    mark.addEventListener("click",function(){
+        
+        //showMarkers();
+        console.log(linePath.length);
+        console.log(markers.length);
+        hideMarkers();
+        markers.pop();
+        linePath.pop();
+        showMarkers();
+        console.log(linePath.length);
+        console.log(markers.length);
+    });
+    
+    // 지도에 표시된 마커 객체를 가지고 있을 배열입니다
+    var markers = [];
+    var linePath = [];
+    // 마커를 생성하고 지도위에 표시하는 함수입니다
+    function addMarker(position) {
+        
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+            position: position,
+            image: markerImage
+        });
+        
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
+        
+        // 생성된 마커를 배열에 추가합니다
+        markers.push(marker);
+        
+        //markers.pop();
+    }
+    var polyline = new kakao.maps.Polyline({
+        path: linePath, // 선을 구성하는 좌표배열 입니다
+        strokeWeight: 5, // 선의 두께 입니다
+        strokeColor: '#FFAE00', // 선의 색깔입니다
+        strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+        strokeStyle: 'solid' // 선의 스타일입니다
+    });
+    function addLin(lat,lng){
+        var lin = new kakao.maps.LatLng(lat,lng);
+        polyline.setMap(map);  
+        linePath.push(lin);
+        
+    }
+
+    // 배열에 추가된 마커들을 지도에 표시하거나 삭제하는 함수입니다
+    function setMarkers(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }                   
+    }
+      
+    // "마커 보이기" 버튼을 클릭하면 호출되어 배열에 추가된 마커를 지도에 표시하는 함수입니다
+    function showMarkers() {
+        setMarkers(map)    
+    }
+
+    // "마커 감추기" 버튼을 클릭하면 호출되어 배열에 추가된 마커를 지도에서 삭제하는 함수입니다
+    function hideMarkers() {
+        setMarkers(null);    
+    }
+
+   
+
+// 주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+
+var marker = new kakao.maps.Marker(), // 클릭한 위치를 표시할 마커입니다
+    infowindow = new kakao.maps.InfoWindow({zindex:1}); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
+
+// 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
+searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+
+// 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
+kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+    searchDetailAddrFromCoords(mouseEvent.latLng, function(result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+            var detailAddr = !!result[0].road_address ? '<div>도로명주소 : ' + result[0].road_address.address_name + '</div>' : '';
+            detailAddr += '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
+            
+            var content = '<div class="bAddr">' +
+                            '<span class="title">법정동 주소정보</span>' + 
+                            detailAddr + 
+                        '</div>';
+
+            // 마커를 클릭한 위치에 표시합니다 
+            marker.setPosition(mouseEvent.latLng);
+            marker.setMap(map);
+
+            // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
+            infowindow.setContent(content);
+            infowindow.open(map, marker);
+        }   
+    });
+});
+
+//============= 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
+    kakao.maps.event.addListener(map, 'idle', function() {
+        searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+    });
+
+    function searchAddrFromCoords(coords, callback) {
+        // 좌표로 행정동 주소 정보를 요청합니다
+        geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);         
+    }
+
+    function searchDetailAddrFromCoords(coords, callback) {
+        // 좌표로 법정동 상세 주소 정보를 요청합니다
+        geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+    }
+
+    // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
+    function displayCenterInfo(result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+            var infoDiv = document.getElementById('centerAddr');
+
+            for(var i = 0; i < result.length; i++) {
+                // 행정동의 region_type 값은 'H' 이므로
+                if (result[i].region_type === 'H') {
+                    infoDiv.innerHTML = result[i].address_name;
+                    break;
+                }
+            }
+        }    
+    }
 
 });
--- src/main/webapp/resource/js/inc/header.js
+++ src/main/webapp/resource/js/inc/header.js
@@ -1,3 +1,17 @@
+
+//사용자 위치정보 위도 경도 
+window.addEventListener("load",findLocation());
+	function findLocation() {
+		if (navigator.geolocation) { 
+			window.navigator.geolocation.getCurrentPosition(showYourLocation); 
+		} else { 
+			loc.innerHTML = "이 문장은 사용자의 웹 브라우저가 Geolocation API를 지원하지 않을 때 나타납니다!"; 
+		}
+	}
+	function showYourLocation(position){
+		var po = "lat : "+ position.coords.latitude +", long :"+ position.coords.longitude;
+		console.log(po);
+	}
 window.addEventListener("load", function() {
 	//커플 페이지 노출
 	var coupleBtn = this.document.querySelector(".couple-btx");
@@ -6,7 +20,8 @@ window.addEventListener("load", function() {
 	var uAtoggle = this.document.querySelector("#user-alert-toggle");
 	var alertBox = this.document.querySelector(".alert-box");
 	var newTable = this.document.querySelector(".alam-table");
-
+	//html gps 라이브러리
+	findLocation();
 	//미세먼지 api
 	// var key = "Vk0LBbN0X7MW7cwfHY1cB%2BDgKhHubcpgmLXX8NFZm0kjKG%2FKBw%2F1z2UgplRmAxnRVOvVn%2FbgzjOTxIJjc7YYGw%3D%3D";
 	// var queryString = "?regId=11A00101&&Servicekey="+key;
